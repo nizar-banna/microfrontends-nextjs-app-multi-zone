c1b70430eb252da88855ae5cd18972ad2a44da77
Nizar Banna <nizar.jalal98@gmail.com>
2025-09-05 01:21:55 +0300

    save

--- apps/marketing-micro-fe/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-import { withMicrofrontends } from '@vercel/microfrontends/next/config';
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default withMicrofrontends(nextConfig, { debug: true });
